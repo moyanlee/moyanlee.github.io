5030dae84753da2308b62e386b576d83096fdca2
moyanlee <90555015+moyanlee@users.noreply.github.com>
2021-11-21 22:46:23 -0500

    Add files via upload

---
+++ Assignment06.js
@@ -0,0 +1,7 @@
+function buttonclick() {
+    alert("there is absolutely no inevitability as long as there is a willingness to contemplate what is happening")
+}
+
+function buttonclick2() {
+    alert("All media are extensions of some human faculty- psychic or physical.")
+}
